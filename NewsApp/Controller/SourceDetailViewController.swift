876d152dfdf38bc34bde159784b69c5ae1d5efd1
sigidhanafi <sigidhanafi@gmail.com>
2018-02-21 23:39:48 +0700

    add dynamic table view cell on article list

--- NewsApp/Controller/SourceDetailViewController.swift
+++ NewsApp/Controller/SourceDetailViewController.swift
@@ -21,41 +21,49 @@ class SourceDetailViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.title = self._title
+        
         tableView.delegate = self
         tableView.dataSource = self
-
-        self.navigationItem.title = self._title
-
-        let provider = MoyaProvider<NewsApiService>()
-        provider.request(.source(sources: self._sourceId, pageSize: 10, page: 1)) { result in
-            switch result {
-                case .success(let response):
-                    let responseJSON = JSON(response.data)
-                    let responseArticles = responseJSON["articles"]
-                    for (key, articleDict) in responseArticles {
-                        let title = articleDict["title"]
-                        let description = articleDict["description"]
-                        let dataArticle: [String: String] =  ["title": "\(title)", "description": "\(description)"]
-                        self.articles.append(dataArticle)
-                    }
-                    self.tableView.reloadData()
-                case .failure(let error):
-                    print("ERRRR", error)
-            }
-        }
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 150
+        
+        loadDataArticles()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleTableViewCell
         let data = self.articles[indexPath.row]
         let title = data["title"]
-        // let description = data["description"]
+        let description = data["description"]
         cell.titleLabel.text = title
+        cell.descriptionLabel.text = description
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
+    }
+    
+    func loadDataArticles() {
+        let provider = MoyaProvider<NewsApiService>()
+        provider.request(.source(sources: self._sourceId, pageSize: 10, page: 1)) { result in
+            switch result {
+            case .success(let response):
+                let responseJSON = JSON(response.data)
+                let responseArticles = responseJSON["articles"]
+                for (key, articleDict) in responseArticles {
+                    let title = articleDict["title"]
+                    let description = articleDict["description"]
+                    let dataArticle: [String: String] =  ["title": "\(title)", "description": "\(description)"]
+                    self.articles.append(dataArticle)
+                }
+                self.tableView.reloadData()
+            case .failure(let error):
+                print("ERRRR", error)
+            }
+        }
     }
 
 }
--- NewsApp/View/ArticleTableViewCell.swift
+++ NewsApp/View/ArticleTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class ArticleTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
