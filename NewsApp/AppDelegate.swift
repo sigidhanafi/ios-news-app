eede2f20edc1a867cd0365d5318f7f00bdcc4502
sigidhanafi <sigidhanafi@gmail.com>
2019-04-26 19:36:22 +0700

    change app delegate

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -17,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        window?.rootViewController = UINavigationController(rootViewController: SourceListViewController())
+        let sourceListViewController = SourceListViewController()
+        let rootNavigationController = UINavigationController(rootViewController: sourceListViewController)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootNavigationController
+        window?.makeKeyAndVisible()
         
         return true
     }
